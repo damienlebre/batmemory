9f1034f6fee32e44f6c759e7a2e9fa4160a17b81
damienlebre <lebre.damien88@gmail.com>
2022-04-26 23:40:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,361 @@
+// Tableau des images de cartes (en double)
+const deckCards = ["1.png", "1.png", "2.png", "2.png", "3.png", "3.png", "4.png", "4.png", "5.png", "5.png", "6.png", "6.png", "7.png", "7.png", "8.png", "8.png", "9.png", "9.png", "10.png", "10.png", "11.png", "11.png", "12.png", "12.png", "13.png", "13.png", "14.png", "14.png"];
+
+// grille de jeux
+// contenant les cartes
+const deck = document.querySelector(".deck");
+// tableau pour stocker les cartes retournées
+let opened = [];
+// tableau pour stocker les pairs qui ont matché
+let matched = [];
+
+// modal
+const modal = document.getElementById("modal");
+
+// boutton reset
+const reset = document.querySelector(".reset-btn");
+// boutton rejouer
+const playAgain = document.querySelector(".play-again-btn");
+
+//cibler div compteur de coup
+const movesCount = document.querySelector(".moves-counter");
+//variable pour compter les coup(part de 0)
+let moves = 0;
+
+// comteur étoile (rank)
+const star = document.getElementById("star-rating").querySelectorAll(".star");
+// Variable de décompte des étoiles
+let starCount = 3;
+
+// cibler le span du timer
+const timeCounter = document.querySelector(".timer");
+// To use this variable to stop the time started in timer
+let time;
+// Create variables for time count, start all at zero
+let minutes = 0;
+let seconds = 0;
+// For use in the click card event listener
+let timeStart = false;
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+/*
+Start Game: mélange le deck, créer les <li> et les  <img> 
+et les distribuent dans les <ul>.
+*/
+function startGame() {
+	// joue la fonction suffle sur le jeux de cartes et stock le resultat dans une  const
+	const shuffledDeck = shuffle(deckCards); 
+	// repartie les carte sur la grille de jeu
+	for (let i = 0; i < shuffledDeck.length; i++) {
+		// création des <li>
+		const liTag = document.createElement('LI');
+		// donne aux <li> la class 'card'
+		liTag.classList.add('card');
+		// Create the <img> tags
+		const addImage = document.createElement("IMG");
+		// créaton balise <img> dans les <li>
+		liTag.appendChild(addImage);
+		// ajoute et renseigne l'attribut src des  balises images
+		addImage.setAttribute("src", "images/images/" + shuffledDeck[i] + "?raw=true");
+		// ajoute et renseigne un attribut alt aux balises <img>
+		addImage.setAttribute("alt", "image of vault boy from fallout");
+		// cible tout les enfants de deck (les éléments du tableau) pour leur ajouter la balise <li> 
+		deck.appendChild(liTag);
+	}
+}
+
+startGame();
+
+/*
+Remove all child nodes from the deck <li> tags and
+<img> tags.  To be called in set everything function only
+*/
+function removeCard() {
+	// As long as <ul> deck has a child node, remove it
+	while (deck.hasChildNodes()) {
+		deck.removeChild(deck.firstChild);
+	}
+}
+
+/*
+récupérer la valeur du timer dans le html
+la fonction est déclenchées a l'eventlistener du click du la premiere carte 
+voir: https://www.w3schools.com/js/js_timing.asp
+*/
+function timer() {
+	// décompte par secondes
+	time = setInterval(function() {
+		seconds++;
+			if (seconds === 60) {
+				minutes++;
+				seconds = 0;
+			}
+		// afficher dans html le temps total de la partie jusqu'a la victoire.
+		timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes + " Mins " + secondes + " Secs" ;
+	}, 1000);
+}
+
+/*
+Arret du timer lorsqu les 14 paires sont découverte
+Used: https://www.w3schools.com/js/js_timing.asp
+*/
+function stopTime() {
+	clearInterval(time);
+}
+
+/*
+Reset all global variables and the content of HTML elements
+timer, stars, moves, and the moves and timer inner HTML
+*/
+function resetEverything() {
+	// Stop time, reset the minutes and seconds update the time inner HTML
+	stopTime();
+	timeStart = false;
+	seconds = 0;
+	minutes = 0;
+	timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
+	// Reset star count and the add the class back to show stars again
+	star[1].firstElementChild.classList.add("fa-star");
+	star[2].firstElementChild.classList.add("fa-star");
+	starCount = 3;
+	// Reset moves count and reset its inner HTML
+	moves = 0;
+	movesCount.innerHTML = 0;
+	// Clear both arrays that hold the opened and matched cards
+	matched = [];
+	opened = [];
+	// Clear the deck
+	removeCard();
+	// Create a new deck
+	startGame();
+}
+
+/*
+Increment the moves counter.  To be called at each
+comparison for every two cards compared add one to the count
+*/
+function movesCounter() {
+	// Update the html for the moves counter
+	movesCount.innerHTML ++;
+	// Keep track of the number of moves for every pair checked
+	moves ++;
+}
+
+/*
+Update the star rating.  Depending on the number of
+moves the user completes the game, the stars will decrease
+with the more moves the user takes.
+*/
+function starRating() {
+	if (moves === 14) {
+		// First element child is the <i> within the <li>
+		star[2].firstElementChild.classList.remove("fa-star");
+		starCount--;
+	}
+	if (moves === 18) {
+		star[1].firstElementChild.classList.remove("fa-star");
+		starCount--;
+	}
+}
+
+/*
+Compare two cards to see if they match or not
+*/
+function compareTwo() {
+	// When there are 2 cards in the opened array
+	if (opened.length === 2) {
+  		// Disable any further mouse clicks on other cards
+  		document.body.style.pointerEvents = "none";
+  }
+	// Compare the two images src
+	if (opened.length === 2 && opened[0].src === opened[1].src) {
+		// If matched call match()
+		match();
+		// console.log("It's a Match!");
+	} else if (opened.length === 2 && opened[0].src != opened[1].src) {
+		// If No match call noMatch()
+		noMatch();
+		// console.log("NO Match!");
+	}
+}
+
+/*
+If the two cards match, keep the cards open and
+apply class of match
+*/ 
+function match() {
+	/* Access the two cards in opened array and add
+	the class of match to the imgages parent: the <li> tag
+	*/
+	setTimeout(function() {
+		opened[0].parentElement.classList.add("match");
+		opened[1].parentElement.classList.add("match");
+		// Push the matched cards to the matched array
+		matched.push(...opened);
+		// Allow for further mouse clicks on cards
+		document.body.style.pointerEvents = "auto";
+		// Check to see if the game has been won with all 8 pairs
+		winGame();
+		// Clear the opened array
+		opened = [];
+	}, 600);
+	// Call movesCounter to increment by one
+	movesCounter();
+	starRating();
+}
+
+/*
+If the two cards do not match, remove the cards
+from the opened array and flip the cards back over by
+removing the flip class.
+*/
+function noMatch() {
+	/* After 700 miliseconds the two cards open will have
+	the class of flip removed from the images parent element <li>*/
+	setTimeout(function() {
+		// Remove class flip on images parent element
+		opened[0].parentElement.classList.remove("flip");
+		opened[1].parentElement.classList.remove("flip");
+		// Allow further mouse clicks on cards
+		document.body.style.pointerEvents = "auto";
+		// Remove the cards from opened array
+		opened = [];
+	}, 700);
+	// Call movesCounter to increment by one
+	movesCounter();
+	starRating();
+}
+
+/*
+Get stats on the time, how many moves, and star rating
+for the end game and update the modal with these stats
+*/
+function AddStats() {
+	// Access the modal content div
+	const stats = document.querySelector(".modal-content");
+	// Create three different paragraphs
+	for (let i = 1; i <= 3; i++) {
+		// Create a new Paragraph
+		const statsElement = document.createElement("p");
+		// Add a class to the new Paragraph
+		statsElement.classList.add("stats");
+		// Add the new created <p> tag to the modal content
+		stats.appendChild(statsElement);
+	}
+	// Select all p tags with the class of stats and update the content
+	let p = stats.querySelectorAll("p.stats");
+			// Set the new <p> to have the content of stats (time, moves and star rating)
+		p[0].innerHTML = "Temps Total: " + minutes + " Minutes et " + seconds + " Secondes";
+		p[1].innerHTML = "coup joués: " + moves;
+		p[2].innerHTML = "Nombres d'étoiles: " + starCount + " sur 3";
+}
+
+/*
+Display the modal on winning the game
+Help with the modal from:
+https://www.w3schools.com/howto/howto_css_modals.asp
+*/
+function displayModal() {
+// Access the modal <span> element (x) that closes the modal
+const modalClose = document.getElementsByClassName("close")[0];
+	// When the game is won set modal to display block to show it
+	modal.style.display= "block";
+
+	// When the user clicks on <span> (x), close the modal
+	modalClose.onclick = function() {
+		modal.style.display = "none";
+	};
+// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	};
+}
+
+/*
+Check the length of the matched array and if there
+are 14 pairs 28 cards all together then the game is won.
+Stop the timer update the modal with stats and show the modal
+*/
+function winGame() {
+	if (matched.length === 28) {
+		stopTime();
+		AddStats();
+		displayModal();
+	}
+}
+
+/*----------------------------------  
+Main Event Listener
+------------------------------------*/
+/*
+Event Listener if a card <li> is clicked
+call flipCard()
+*/
+deck.addEventListener("click", function(evt) {
+	if (evt.target.nodeName === "LI") {
+		// To console if I was clicking the correct element 
+		console.log(evt.target.nodeName + " Was clicked");
+		// Start the timer after the first click of one card
+	// Executes the timer() function
+		if (timeStart === false) {
+			timeStart = true; 
+			timer();
+		}
+		// Call flipCard() function
+		flipCard();
+	}
+
+	//Flip the card and display cards img
+	function flipCard() {
+		// When <li> is clicked add the class .flip to show img
+		evt.target.classList.add("flip");
+		// Call addToOpened() function
+		addToOpened();
+	}
+	 
+	//Add the fliped cards to the empty array of opened
+	function addToOpened() {
+		/* If the opened array has zero or one other img push another 
+		img into the array so we can compare these two to be matched
+		*/
+		if (opened.length === 0 || opened.length === 1) {
+			// Push that img to opened array
+			opened.push(evt.target.firstElementChild);
+		}
+		// Call compareTwo() function
+		compareTwo();
+	}
+}); //Event Listener
+
+/*----------------------------------  
+Restart Buttons
+------------------------------------*/
+/*
+Event Listener to listen for a click on the reset
+button, once clicked call resetEverything()
+*/
+reset.addEventListener('click', resetEverything);
+
+/*
+Event Listener to listen for a click on the play
+again button, once clicked call resetEverything()
+*/
+playAgain.addEventListener('click',function() {
+	modal.style.display = "none";
+	resetEverything();
+});
